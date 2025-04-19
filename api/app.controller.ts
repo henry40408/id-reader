72d13c72cea2b212560c2361958085eff282eac9
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2025-04-19 12:33:35 +0800

    chore: swagger

--- api/app.controller.ts
+++ api/app.controller.ts
@@ -1,16 +1,20 @@
 import { All, Controller, Get, NotFoundException, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 import { ViteService } from './vite.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly viteService: ViteService) {}
 
+  @ApiOperation({ summary: 'check health' })
+  @ApiOkResponse({ description: 'service is up' })
   @Get('healthz')
   healthz() {
     return '';
   }
 
+  @ApiOperation({ summary: 'catch-all endpoint to Vite dev server in development environment' })
   @All('*')
   catchCall(@Req() req: Request, @Res() res: Response) {
     const middlewares = this.viteService.middlewares;
--- api/main.ts
+++ api/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerDocumentOptions, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  {
+    const config = new DocumentBuilder()
+      .setTitle('id reader')
+      .setDescription('A RSS reader')
+      .setVersion('0.0.0-dev')
+      .build();
+    const options: SwaggerDocumentOptions = {
+      autoTagControllers: true,
+    };
+    const documentFactory = () => SwaggerModule.createDocument(app, config, options);
+    SwaggerModule.setup('api', app, documentFactory);
+  }
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch(console.error);
