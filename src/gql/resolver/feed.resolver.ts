39d5da5400538d4ba2321eaab851c11718cc5b32
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2025-05-12 08:11:00 +0000

    fix: feed id in feed resolver

--- src/gql/resolver/feed.resolver.ts
+++ src/gql/resolver/feed.resolver.ts
@@ -1,6 +1,7 @@
 import { Context, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
 import { Knex } from 'knex';
 import { Inject } from '@nestjs/common';
+import { Feed } from 'knex/types/tables';
 import { KNEX } from '../../knex/knex.constant';
 import { IGqlContext } from '../gql.interface';
 import { CategoryObject, FeedObject, RequestWithJwtPayload } from '../dtos.interface';
@@ -15,9 +16,10 @@ export class FeedResolver {
   async feeds(@Context() context: IGqlContext<RequestWithJwtPayload>) {
     const userId = context.req.jwtPayload.sub;
     return await this.knex('feeds')
-      .select('*')
+      .select<Feed>('feeds.*')
       .join('categories', 'categories.id', 'feeds.category_id')
-      .where('categories.user_id', userId);
+      .where('categories.user_id', userId)
+      .orderBy('id');
   }
 
   @ResolveField(() => CategoryObject, { description: 'Get category' })
