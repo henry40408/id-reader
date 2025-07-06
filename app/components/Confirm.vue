adee253f0d22c2366869a0fed3f1999eb00c0a51
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2025-07-06 23:54:31 +0800

    feat: add confirmation dialog for category deletion and implement category creation form

---
+++ app/components/Confirm.vue
@@ -0,0 +1,31 @@
+<template>
+  <span>
+    <span v-if="!confirming">
+      <a href="#" @click.prevent="confirming = true"><slot /></a>
+    </span>
+    <span v-else>
+      <span>Are you sure?</span>
+      {{ ' ' }}
+      <button @click="confirm()">Yes</button>
+      {{ ' ' }}
+      <button @click="cancel()">No</button>
+    </span>
+  </span>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const confirming = ref(false);
+const emit = defineEmits(['confirmed', 'cancelled']);
+
+function confirm() {
+  confirming.value = false;
+  emit('confirmed');
+}
+
+function cancel() {
+  confirming.value = false;
+  emit('cancelled');
+}
+</script>
--- app/views/Feeds.vue
+++ app/views/Feeds.vue
@@ -5,11 +5,27 @@
   </header>
   <main>
     <Authenticated>
+      <div>
+        <a href="#" @click="creatingCategory = !creatingCategory">+ Category</a>
+      </div>
+      <div v-if="creatingCategory">
+        <form @submit.prevent="createCategory">
+          <p>
+            <input v-model="categoryName" type="text" placeholder="Category Name" required />
+            {{ ' ' }}
+            <button type="submit">Create Category</button>
+          </p>
+        </form>
+      </div>
       <div v-if="loading">Loading categories...</div>
       <div v-if="error">Error loading categories: {{ error.message }}</div>
       <ul v-if="result && result.getCategories">
         <li v-for="category in result.getCategories" :key="category.id">
-          <div>{{ category.name }}</div>
+          <div>
+            <span>{{ category.name }}</span>
+            {{ ' ' }}
+            <Confirm @confirmed="deleteCategory(category.id)">delete</Confirm>
+          </div>
           <ul>
             <li v-for="feed in category.feeds" :key="feed.id">{{ feed.title }}</li>
           </ul>
@@ -20,10 +36,55 @@
 </template>
 
 <script setup lang="ts">
-import { useQuery } from '@vue/apollo-composable';
+import { useMutation, useQuery } from '@vue/apollo-composable';
 import Authenticated from '../components/Authenticated.vue';
 import Nav from '../components/Nav.vue';
 import { graphql } from '../../generated/gql/gql';
+import { ref } from 'vue';
+import { ApolloError } from '@apollo/client/errors';
+import Confirm from '../components/Confirm.vue';
+
+const creatingCategory = ref(false);
+const categoryName = ref('');
+const error = ref<ApolloError | null>(null);
+
+const createCategoryMutation = graphql(`
+  mutation CreateCategory($name: String!) {
+    createCategory(name: $name) {
+      id
+      name
+    }
+  }
+`);
+const { mutate: doCreateCategory } = useMutation(createCategoryMutation);
+
+const deleteCategoryMutation = graphql(`
+  mutation DeleteCategory($id: Int!) {
+    deleteCategory(id: $id)
+  }
+`);
+const { mutate: doDeleteCategory } = useMutation(deleteCategoryMutation);
+
+async function createCategory() {
+  try {
+    await doCreateCategory({ name: categoryName.value });
+    await refetch();
+    categoryName.value = '';
+  } catch (err: unknown) {
+    console.error('Error creating category:', err);
+    error.value = err as ApolloError;
+  }
+}
+
+async function deleteCategory(id: number) {
+  try {
+    await doDeleteCategory({ id });
+    await refetch();
+  } catch (err: unknown) {
+    console.error('Error deleting category:', err);
+    error.value = err as ApolloError;
+  }
+}
 
 const getCategoriesQuery = graphql(`
   query GetCategories {
@@ -38,5 +99,9 @@ const getCategoriesQuery = graphql(`
   }
 `);
 
-const { result, loading, error } = useQuery(getCategoriesQuery);
+const { result, loading, onError, refetch } = useQuery(getCategoriesQuery);
+onError((err) => {
+  error.value = err;
+  console.error('Error fetching categories:', err);
+});
 </script>
